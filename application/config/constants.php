6f7c9eb1085554358f5efee6510d537899c7923a
Muhammad Rizqi Nur <rizqinur2010@gmail.com>
2024-11-17 01:50:55 +0700

    Update constants.php

--- application/config/constants.php
+++ application/config/constants.php
@@ -84,7 +84,8 @@ defined('EXIT_DATABASE')       OR define('EXIT_DATABASE', 8); // database error
 defined('EXIT__AUTO_MIN')      OR define('EXIT__AUTO_MIN', 9); // lowest automatically-assigned error code
 defined('EXIT__AUTO_MAX')      OR define('EXIT__AUTO_MAX', 125); // highest automatically-assigned error code
 
-define('SITE_NAME', 'Album B');
+defined('SITE_NAME') OR define('SITE_NAME', 'Album B');
+
 $image_path = 'assets/uploads/foto';
 if (ENVIRONMENT == 'production'){
     $image_path = $_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . $image_path;
@@ -95,7 +96,7 @@ if (ENVIRONMENT == 'production'){
         $image_path =  sys_get_temp_dir();
     }
 }
-define('IMAGE_PATH', $image_path);
+defined('IMAGE_PATH') OR define('IMAGE_PATH', $image_path);
 
 defined('_PROCESS_TEST_') OR define('_PROCESS_TEST_', FALSE);
 defined('CACHE_KEY') OR define('CACHE_KEY', 'Bear-CodeIgniter-v3-Skeleton-on-Vercel-Cache-');
