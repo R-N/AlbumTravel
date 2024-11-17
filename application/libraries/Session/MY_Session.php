eb87024f2c6e1dd753d343690f8bf4711bf88d84
Muhammad Rizqi Nur <rizqinur2010@gmail.com>
2024-11-17 16:12:30 +0700

    Update MY_Session.php

--- application/libraries/Session/MY_Session.php
+++ application/libraries/Session/MY_Session.php
@@ -2,4 +2,13 @@
 
 #[\AllowDynamicProperties]
 class MY_Session extends CI_Session {
+    public function sess_update()
+    {
+        $CI =& get_instance();
+
+        if ( ! $CI->input->is_ajax_request())
+        {
+            parent::sess_update();
+        }
+    }
 }
